13c45260532021e553725b2557a24c626f9d9dba
Chris Whong <chris@whong.org>
2017-07-26 15:58:01 -0400

    fix merge conflict

--- routes/projects.js
+++ routes/projects.js
@@ -2,22 +2,10 @@ const express = require('express');
 const request = require('request');
 const querystring = require('querystring');
 const slug = require('slug');
-const whitelisted_fields = 
-  process.env.WHITELISTED_FIELDS.split(',') ||
-  ['customer','problem_statement','project_id','project_name','project_types','short_description','strategic_objectives'];
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-<<<<<<< Updated upstream
-  const fields = querystring.stringify({fields: whitelisted_fields});
-  const url = `
-    https://api.airtable.com/v0/app1f3lv9mx7L5xnY/Labs Project Tracking?${fields}&
-    view=All Projects&api_key=${process.env.AIRTABLE_API_KEY}
-  `;
-
-  console.log(url);
-=======
   const whitelisted_fields =
     ['customer', 'problem_statement', 'project_id', 'project_name', 'tags', 'short_description', 'strategic_objectives'];
   const fields = querystring.stringify({ fields: whitelisted_fields });
@@ -25,7 +13,6 @@ router.get('/', (req, res) => {
 
   console.log(`Fetching ${url}`); // eslint-disable-line
 
->>>>>>> Stashed changes
   request({ url, json: true }, (err, response, body) => {
     const newArray = body.records.map(obj => obj.fields);
 
