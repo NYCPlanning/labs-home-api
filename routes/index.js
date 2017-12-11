d3c7af7c292444e304df0738a3961dcd8d2a4fa9
allthesignals <wilburnforce@gmail.com>
2017-12-11 13:55:57 -0500

    Add new endpoint for public, vetted, active projec

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const ideas = require('./ideas');
 const projects = require('./projects');
 
 const router = express.Router();
 
-router.use('/ideas', projects);
+router.use('/ideas', ideas);
+router.use('/projects', projects);
 
 module.exports = router;
