fc87b5481a472f12d393f37abf85aa51fb986882
Chris Whong <chris@whong.org>
2017-12-12 00:31:50 -0500

    merge master

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const ideas = require('./ideas');
 const projects = require('./projects');
+const posts = require('./posts');
 
 const router = express.Router();
 
 router.use('/ideas', ideas);
 router.use('/projects', projects);
+router.use('/posts', posts);
 
 module.exports = router;
