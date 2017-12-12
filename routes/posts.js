6c4326e0af9ed29e980c776fb9a1092d8cbdae32
Chris Whong <chris@whong.org>
2017-12-12 01:10:16 -0500

    parse medium description text

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const Feed = require('rss-to-json');
+const cheerio = require('cheerio');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   Feed.load('https://medium.com/feed/nycplanninglabs?truncated=true', (err, rss) => {
+    rss.items.map((item) => {
+      const $ = cheerio.load(item.description);
+      const parsedDescription = $('.medium-feed-snippet').text();
+
+      const newItem = item;
+      newItem.description = parsedDescription;
+      return newItem;
+    });
+
     res.json(rss);
   });
 });
