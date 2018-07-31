443e60b4acdc48ff91f1fa54acaf31b939b27161
Andy Cochran <acochran@planning.nyc.gov>
2018-07-31 15:32:16 -0400

    add user-agent to CORS header

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization', 'user-agent');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,user-agent');
   next();
 });
 
