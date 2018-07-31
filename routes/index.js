e1aecff5e84efcfebe67c06973fed72128d24bb1
Andy Cochran <acochran@planning.nyc.gov>
2018-07-31 16:01:52 -0400

    nix ideas endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const ideas = require('./ideas');
 const projects = require('./projects');
 const posts = require('./posts');
 
 const router = express.Router();
 
-router.use('/ideas', ideas);
 router.use('/projects', projects);
 router.use('/posts', posts);
 
