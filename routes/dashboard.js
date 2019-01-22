7e1846879e8df82dadfd3694154f9c5b99e03e33
trbmcginnis <taylormcgin@gmail.com>
2019-01-22 14:53:17 -0500

    fix failing linting tests

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,9 +3,6 @@ const request = require('request');
 const mapObj = require('map-obj');
 const camelCase = require('camelcase');
 const kebabCase = require('kebab-case');
-const fetch = require('node-fetch');
-const octokit = require('@octokit/rest')();
-const graphql = require('@octokit/graphql');
 
 const developMasterSyncWidget = require('../widgets/develop-master-sync');
 
@@ -87,8 +84,7 @@ router.get('/repos/:id', (req, res) => {
             },
           });
         })
-        .catch((err) => {
-          console.log('CAUGHT IN ALL');
+        .catch(() => {
         });
 });
 
