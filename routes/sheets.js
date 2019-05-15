3bb62f9584adbcdadb30a4e7ddcfe088f0bc25eb
tommyswang <tommyswang@gmail.com>
2019-05-15 12:15:15 -0400

    Extracted the keys from JSON file and created new environment variables

--- routes/sheets.js
+++ routes/sheets.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
-const creds = require('../client_secret.json');
 
 const router = express.Router();
 
@@ -16,7 +15,18 @@ router.get('/', async (req, res) => {
 
   const projectsJSON = [];
   const doc = new GoogleSpreadsheet(`${process.env.GOOGLE_SHEETS_ID}`);
-  await promisify(doc.useServiceAccountAuth)(creds);
+  await promisify(doc.useServiceAccountAuth)({
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  });
   const info = await promisify(doc.getInfo)();
   const sheet = info.worksheets[0];
   const rows = await promisify(sheet.getRows)({
